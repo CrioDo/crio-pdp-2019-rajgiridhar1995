43a92784af43d3871019d617d418576c15feedcc
Anas Khan <anas.khan96@outlook.com>
2020-03-04 19:11:34 +0530

    initial commit

---
+++ precache-manifest.596fcf803be9b94b575254c7fd759494.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97fb2d9576c05bdcdd177ef9e7929c78",
+    "url": "/crio-pdp-2019-rajgiridhar1995/index.html"
+  },
+  {
+    "revision": "b2698926c2f46fb8e393",
+    "url": "/crio-pdp-2019-rajgiridhar1995/static/css/2.b3ff1c62.chunk.css"
+  },
+  {
+    "revision": "0e13f392c3240b0abeee",
+    "url": "/crio-pdp-2019-rajgiridhar1995/static/css/main.1c86f277.chunk.css"
+  },
+  {
+    "revision": "b2698926c2f46fb8e393",
+    "url": "/crio-pdp-2019-rajgiridhar1995/static/js/2.5bd4ade0.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/crio-pdp-2019-rajgiridhar1995/static/js/2.5bd4ade0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e13f392c3240b0abeee",
+    "url": "/crio-pdp-2019-rajgiridhar1995/static/js/main.e5c2c8fb.chunk.js"
+  },
+  {
+    "revision": "d254b04b2e2d99b78c58",
+    "url": "/crio-pdp-2019-rajgiridhar1995/static/js/runtime-main.6ed2bed8.js"
+  },
+  {
+    "revision": "c9536e0497f939475da3110091e1a10d",
+    "url": "/crio-pdp-2019-rajgiridhar1995/static/media/crio-logo-white.c9536e04.svg"
+  },
+  {
+    "revision": "63be71d01edef2dcf3cafba5e52553a4",
+    "url": "/crio-pdp-2019-rajgiridhar1995/static/media/crio-logo.63be71d0.svg"
+  },
+  {
+    "revision": "d562384718049148dee6f282fd304a08",
+    "url": "/crio-pdp-2019-rajgiridhar1995/static/media/mil3biryani.d5623847.png"
+  },
+  {
+    "revision": "b9446ae49761e6908c20a3397905d590",
+    "url": "/crio-pdp-2019-rajgiridhar1995/static/media/mil3idly.b9446ae4.png"
+  },
+  {
+    "revision": "b87695eab7483ddd4078d2712f145d82",
+    "url": "/crio-pdp-2019-rajgiridhar1995/static/media/mil3south.b87695ea.png"
+  },
+  {
+    "revision": "d2020dcc80f4f5f69daddc53a890e584",
+    "url": "/crio-pdp-2019-rajgiridhar1995/static/media/mil51item.d2020dcc.png"
+  },
+  {
+    "revision": "1f6ad86730b2efc5addbb2440c931757",
+    "url": "/crio-pdp-2019-rajgiridhar1995/static/media/mil52item.1f6ad867.png"
+  },
+  {
+    "revision": "c904eb2c76a2d98e280c021f301a983f",
+    "url": "/crio-pdp-2019-rajgiridhar1995/static/media/mil53item.c904eb2c.png"
+  },
+  {
+    "revision": "8c20972f72cac52ddcd42a6d2b0f14eb",
+    "url": "/crio-pdp-2019-rajgiridhar1995/static/media/mil5cart.8c20972f.png"
+  },
+  {
+    "revision": "ae176d5cbeb608c0bfc0582b4e93f7fe",
+    "url": "/crio-pdp-2019-rajgiridhar1995/static/media/mil5order.ae176d5c.png"
+  },
+  {
+    "revision": "ace76abdddb73524807673c7530f2dcf",
+    "url": "/crio-pdp-2019-rajgiridhar1995/static/media/milestone1.ace76abd.png"
+  },
+  {
+    "revision": "95983c4e0260ad3ad49776f49cc1c041",
+    "url": "/crio-pdp-2019-rajgiridhar1995/static/media/milestone2.95983c4e.png"
+  },
+  {
+    "revision": "53520bea716a1aaf1cf318f0b14450ab",
+    "url": "/crio-pdp-2019-rajgiridhar1995/static/media/qeatsperf.53520bea.gif"
+  },
+  {
+    "revision": "b5f8c7f6fcd422ab29ff3ed2289d7b99",
+    "url": "/crio-pdp-2019-rajgiridhar1995/static/media/qmoney.b5f8c7f6.png"
+  },
+  {
+    "revision": "4f182a31717ea852a39ec2b8cc048d09",
+    "url": "/crio-pdp-2019-rajgiridhar1995/static/media/start.4f182a31.png"
+  }
+]);
